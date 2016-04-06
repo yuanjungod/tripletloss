9466e545014ca4412e40909e345506d82d2a849b
luhaofang <luhaofang@camera360.com>
2016-04-06 17:56:23 +0800

    no residual array fix

--- layer/tripletlayer.py
+++ layer/tripletlayer.py
@@ -77,9 +77,9 @@ class TripletLayer(caffe.Layer):
                 bottom[0].diff[i*3+1] =  ((x_p - x_a)/((bottom[0]).num/3))
                 bottom[0].diff[i*3+2] =  ((x_a - x_n)/((bottom[0]).num/3))
             else:
-                bottom[0].diff[i*3] = 0
-                bottom[0].diff[i*3+1] = 0
-                bottom[0].diff[i*3+2] = 0
+                bottom[0].diff[i*3] = np.zeros(shape(bottom[0].data)[1])
+                bottom[0].diff[i*3+1] = np.zeros(shape(bottom[0].data)[1])
+                bottom[0].diff[i*3+2] = np.zeros(shape(bottom[0].data)[1])
 
     def reshape(self, bottom, top):
         """Reshaping happens during the call to forward."""
