5713efa7800714820ab3aad3db797dd2392a9114
David Lu <luhaofang@camera360.com>
2016-05-10 14:03:58 +0800

    Update config.py

--- tripletloss/config.py
+++ tripletloss/config.py
@@ -8,7 +8,7 @@ IMAGEPATH = '/home/seal/dataset/fast-rcnn/caffe-fast-rcnn/data/Facedevkit/triple
 SNAPSHOT_ITERS = 10000
 
 # Max training iteration
-MAX_ITERS = 400000
+MAX_ITERS = 40000
 
 # The number of postive samples in each minibatch, which need to be finetuned in your database
 POSITIVE_NUM = 5
